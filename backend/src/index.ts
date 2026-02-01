bbcdf5da66b98bbdd22635e354fa50371e0086d4
ankush-github-11 <ankush10yt@gmail.com>
2026-02-02 02:31:25 +0530

    Fixing cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,8 +23,7 @@ app.use(
       "http://localhost:5173",
       "https://libman-system.vercel.app"
     ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
